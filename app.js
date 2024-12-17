8cb7159822f8be7e76cabff0f34d4beda24bcc38
LordAnthrax94 <andreagi1994@gmail.com>
2024-12-17 16:35:26 +0100

    fix: aggiornati nomi file

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const myPost = require('./post');
+const myPost = require('./bacheca');
 
 app.use(express.static('Public'));
 
-app.get('/', (res, req)=>{
+app.get('/bacheca', (res, req)=>{
   res.json(myPost)
 })
 
 app.listen(port, ()=>{
   console.log(`Sono in ascolto per il server sulla porta ${port}`);  
 })
+
